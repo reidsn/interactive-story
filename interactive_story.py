b17a4a9d3b12a7c6b606eb6133adf1157a8679ec
reidsn <60250275+reidsn@users.noreply.github.com>
2023-01-28 17:27:44 -0800

    Adjust run script for json and classifier

--- interactive_story.py
+++ interactive_story.py
@@ -1,192 +1,116 @@
-class Scene:
-    ## Models a scene with a script and two outcome pointers.
-
-    ## Constructs a scene object with script lines and outcomes.
-    # @param script a list of script lines
-    # @param outcomes a list of outcome pointers
-    #
-    def __init__(self, script, qtype, question, outcomes, replace):
-        self.script = script
-        self.qtype = qtype
-        self.question = question
-        self.outcomes = outcomes
-        self.replace = replace
-
-    ## Sets the question type.
-    # @param qtype the qyestion type
-    #
-    def set_qtype(self, qtype):
-        self.qtype = qtype
-
-    ## Sets the script.
-    # @param script the script
-    #
-    def set_script(self, script):
-        self.script = script
-
-    ## Sets the question.
-    # @param question the question
-    #
-    def set_question(self, question):
-        self.question = question
-
-    ## Gets the script.
-    # @return the script
-    #
-    def get_script(self):
-        return self.script
-
-    ## Gets the question.
-    # @return the question
-    #
-    def get_question(self):
-        return self.question
-
-    ## Gets the replace tags.
-    # @return the replace tags
-    def get_replace(self):
-        return self.replace
-
-    ## Runs the scene.
-    # @return the outcome scene to go to
-    #
-    def run_scene(self):
-        print('-' * 79)
-        for line in self.script:
-                print(line)
-        if self.qtype != 'random':
-            question = input(self.question + '\n')
-            print('\n')
-        else:
-            question = self.question.split(',')
-            for i in range(len(question)):
-                question[i] = int(question[i])
-        return answer(question, self.qtype, self.outcomes)
-
-    def to_string(self):
-        return('Script: ' + str(self.script) + '\nType: ' + self.qtype + \
-               '\nOutcomes: ' + str(self.outcomes))
-
-
-        
-## Determines what outcome the user answer indicates.
-# @param text the user input
-# @param qtype the type of question
-# @return a boolean indicating which outcome is appropriate
-def answer(text, qtype, outcomes):
-    if qtype == 'yesno': #judges for yes/no questions
-        THRESHOLD_PCT = 0.75
-        THRESHOLD_TOTAL = 10
-        unknown_words = []
-        uncountable_words = []
-        ambiguous_words = []
-
-        while (word_dict.get(text) is None) or \
-            (sum(word_dict.get(text)) < THRESHOLD_TOTAL) or \
-            ((word_dict.get(text)[0]/sum(word_dict.get(text))) < THRESHOLD_PCT and \
-            (word_dict.get(text)[1]/sum(word_dict.get(text))) < THRESHOLD_PCT):
-            if word_dict.get(text) is None:
-                unknown_words.append(text)
-            elif sum(word_dict.get(text)) < THRESHOLD_TOTAL:
-                uncountable_words.append(text)
-            elif (word_dict.get(text)[0]/sum(word_dict.get(text))) < THRESHOLD_PCT and \
-                (word_dict.get(text)[1]/sum(word_dict.get(text))) < THRESHOLD_PCT:
-                ambiguous_words.append(text)
-
-            text = input("Is that yes or no?")
-
-        counts = word_dict.get(text)
-        pos = counts[0] / (counts[0] + counts[1])
-        if pos >= THRESHOLD_PCT:
-            index = 0
-        else:
-            index = 1
-
-        if len(unknown_words) > 0:
-            for word in unknown_words:
-                word_dict[word] = [0,0]
-                (word_dict.get(word))[index] = 1
-        if len(uncountable_words) > 0:
-            for word in uncountable_words:
-                (word_dict.get(word))[index] = (word_dict.get(word))[index] + 1
-        if len(ambiguous_words) > 0:
-            for word in ambiguous_words:
-                (word_dict.get(word))[index] = (word_dict.get(word))[index] + 1
-
-        return outcomes[index]
-    elif 'input' in qtype: #judges for character data
-        if qtype[2] == 'yes':
-            replace_inpt(qtype[1], text)
-        if len(qtype) == 4:
-            scenes.get(qtype[3]).set_qtype(text)
-        return outcomes[0]
-    elif qtype == 'random': #controls randomly selected question options
-        import random
-        return random.choices(outcomes, text)[0]
-    elif qtype == 'choice':
-        while text.upper() not in outcomes:
-            text = input('Be clearer! Which option do you pick?')
-        return text.upper()
-    else: #judges for text matching questions
-        if isinstance(qtype, str) and (qtype.lower() in text.lower()):
-            return outcomes[0]
-        elif isinstance(qtype, list) and (text.lower() in qtype):
-            return outcomes[0]
-        else:
-            return outcomes[1]
-
-def replace_inpt(old, new):
-    for key in scenes:
-        scene = scenes.get(key)
-        replace = scene.get_replace()
-        
-        if old in replace:
-            question = scene.get_question()
-            scene.set_question(question.replace(old, new))
-            
-            script = scene.get_script()
-            new_script = script
-            for i in range(len(script)):
-                new_script[i] = script[i].replace(old, new)
-            scene.set_script(new_script)
-
-def main():
-    next = 'INTRO'
-    while next != 'END':
-        next = (scenes.get(next)).run_scene()
-    for line in endline:
-        print(line)
-
-
-
-# Reads in yes/no data from file.
+"""Command line implementation of Interactive Story"""
 import json
-with open("yesno_data.json", "r") as infile:
-    word_dict = json.load(infile)
-infile.close()
+import pickle
+import random
+import sys
 
-# Reads in and processes scenes from file.
-infile = open('holygrail_story.txt', 'r', encoding='utf-8')
-scenes_list = infile.read().split('##')
-scenes = {}
-for i in range(1, len(scenes_list) - 1):
-    lines = scenes_list[i].rstrip().split('\n')
-    key = lines.pop(0)
-    replace = lines.pop(0)
-    outcome = lines.pop().split(':')
-    qtype = lines.pop()
-    if ',' in qtype:
-        qtype = qtype.split(',')
-    question = lines.pop()
-    scene_data = Scene(lines, qtype, question, outcome, replace)
-    scenes[key] = scene_data
-endline = (scenes_list[len(scenes_list) - 1].split('\n'))[1:]
-infile.close()
+# Set up state variables.
+inventory = dict()
+history = []
 
-# Creates player character and runs program.
-main()
-    
-# Writes new yes/no data to file.
-with open("yesno_data.json", "w") as outfile:
-    json.dump(word_dict, outfile, indent=4)
-outfile.close()
+# Load story.
+filename = sys.argv[1]
+story = json.load(open(filename))
+
+# Load answer classifier.
+with open('model/classifier', 'rb') as picklefile:
+    classifier = pickle.load(picklefile)
+with open('model/vectorizer', 'rb') as picklefile:
+    vectorizer = pickle.load(picklefile)
+
+# Print welcome statement.
+print(
+    "\nWelcome to the Interactive Story!\n\nType your answer and press enter to move to the next scene, or type " \
+    "'quit' to exit the game.\n\nIf you want to go back in the story, type 'back', or 'inventory' to view current "\
+    f"character information.{'-' * 79}\n\n"
+)
+
+# Run story loop.
+key = "intro"
+while key != "end":
+    # Get scene info.
+    scene = story.get(key, story["error"])
+
+    # Handle history update.
+    if key == "intro":
+        history = []
+    history.append(key)
+
+    # Process scene display by scene qtype.
+    if scene["qtype"] == "none":
+        # For 'none' qtype, display scene text and move on.
+        print(f'{scene["scene"]}\n'.format(**inventory))
+        key = scene.get("next", "error")
+    elif scene["qtype"] == "random":
+        # For 'random' qtype, display scene text if filled, then make random choice of next scene from options.
+        if scene["scene"]:
+            print(f'{scene["scene"]}\n'.format(**inventory))
+        key = random.choices(scene["options"], scene["weights"])[0]
+    else:
+        # For 'choice' qtype, collect choices to display to user.
+        choices = f'[{", ".join(scene["choices"])}]\n' if scene["qtype"] == "choice" else ""
+
+        # For all other qtypes, display scene text and collect answer input from user.
+        answer = input(f'{scene["scene"]}\n{choices}'.format(**inventory))
+
+        # Classify user input.
+        vect = vectorizer.transform([answer.lower()]).toarray()
+        label = classifier.predict(vect)[0].lower()
+
+        # Process user input.
+        if answer.lower() == "quit":
+            # Break loop and end program if user quits.
+            break
+        elif label == "back":
+            # Process user request to go back in story.
+            print('-' * 79)
+
+            # Create readable history list and scene key list.
+            history_readable = [story[i]["human_readable"] for i in history if story[i].get("human_readable")]
+            history_keys = [i for i in history if story[i].get("human_readable")]
+
+            # Gather user choice on scene.
+            new_scene = input(f'Please select a scene to return to:\n[{", ".join(history_readable)}]\n').lower()
+
+            # Set scene key and revise history.
+            key = history_keys[[i.lower() for i in history_readable].index(new_scene)]
+            history = history[:history.index(key)]
+        elif label == "inventory":
+            # Process user request to view player inventory.
+            print('-' * 79)
+
+            # Format inventory and display to user.
+            invent_format = '\n'.join([f"{k}: {v}" for k, v in inventory.items()])
+            cont = input(
+                f"Here's the current game information:\n{invent_format}\nHit Enter to close and continue with " \
+                "the story.\n"
+            )
+        elif scene["qtype"] == "input":
+            # For 'input' qtype, save input to inventory.
+            inventory[scene["input"]] = answer
+            key = scene.get("next", "error")
+        elif scene["qtype"] == "yes_no":
+            # For 'yes_no' qtype, use classification result to move to next scene.
+            key = scene.get(label, "error")
+        elif scene["qtype"] == "choice":
+            # For 'choice' qtype, use user input to move to next scene.
+            if answer.lower() not in [i.lower() for i in scene["choices"]]:
+                answer = input("Be clearer! Which option do you pick?\n")
+            key = answer.lower()
+        elif scene["qtype"] == "knowledge_check":
+            # For 'knowledge_check' qtype, check input against answer in inventory.
+            if answer.lower() == inventory[scene["info"]].lower():
+                key = scene.get("correct", "error")
+            else:
+                key = scene.get("wrong", "error")
+        elif scene["qtype"] == "contains":
+            # For 'contains' qtype, check for keywords in user input.
+            if any(i in answer.lower() for i in scene["keywords"]):
+                key = scene.get("contains", "error")
+            else:
+                key = scene.get("not_contains", "error")
+    print('-' * 79)
+
+    # Add add_to_bag item to user inventory['inventory'].
+    if scene.get("add_to_bag"):
+        inventory["inventory"] = inventory.get("inventory", []) + [scene["add_to_bag"]]
